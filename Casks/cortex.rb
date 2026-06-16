5ca0fa7b9865526fd6300ec1209e1ed894c27b83
Aidan McConnon <aidanmcconnon210@gmail.com>
2026-06-16 22:46:00 +0000

    cortex 1.0.22

--- Casks/cortex.rb
+++ Casks/cortex.rb
@@ -5,14 +5,14 @@
 # Bump `version` + both sha256 on each release:
 #   shasum -a 256 Cortex_1.0.1_aarch64.dmg
 cask "cortex" do
-  version "1.0.21"
+  version "1.0.22"
 
   on_arm do
-    sha256 "e20fdc51f8c0f726a26401442b9b1348131e8a701e85f99d062d83cf5fe04b7b"
+    sha256 "cae60e6075e76f1bac07d4926086257318b1f4f9a7d4148c5f61d25322387e05"
     url "https://github.com/PndaMan/cortex/releases/download/v#{version}/Cortex_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "93e1e9380c4fb57c4710f36a4d678b4d3aebe0f7e12d8d29de4d8873c52ee378"
+    sha256 "749dd73209c5428a309345c22655d4d341da8e4c5425d664a4f349c5b6194c93"
     url "https://github.com/PndaMan/cortex/releases/download/v#{version}/Cortex_#{version}_x64.dmg"
   end
 
